4e7af4a1828e1b5bb36cb8a285d7ee1b46c57640
TranAnh05 <anh900472@gmail.com>
2026-07-07 20:26:45 +0700

    Optimize the file structure of BoardContent

--- src/pages/Boards/BoardBar/BoardBar.jsx
+++ src/pages/Boards/BoardBar/BoardBar.jsx
@@ -38,7 +38,6 @@ function BoardBar() {
       gap: 2,
       overflowX: 'auto',
       bgcolor: ( theme ) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
-      borderBottom: '1px solid white'
     }}>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <Chip
