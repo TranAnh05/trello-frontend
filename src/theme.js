d74858f64e0dca3b0f9d9cb0f7d1a61ee3224b62
TranAnh05 <anh900472@gmail.com>
2026-07-07 16:27:40 +0700

    Code Board Content part 1

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,14 @@
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
+const APP_BAR_HEIGHT = '62px'
+const BOARD_BAR_HEIGHT = '62px'
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
+
 const theme = extendTheme({
   trello: {
-    appBarHeight: '62px',
-    boardBarHeight: '62px'
+    appBarHeight: APP_BAR_HEIGHT,
+    boardBarHeight: BOARD_BAR_HEIGHT,
+    boardContentHeight: BOARD_CONTENT_HEIGHT
   },
   colorSchemes: {
     light: {},
@@ -53,6 +58,13 @@ const theme = extendTheme({
       styleOverrides: {
         root: {
           fontSize: '0.875rem'
+        }
+      }
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          '&.MuiTypography-body1': { fontSize: '0.875rem' }
         }
       }
     }
